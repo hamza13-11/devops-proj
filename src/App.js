8e4a3a88eae6034c091b59621aa6ffe7d33f376c
hamza13-11 <hamza.ahmad@camp2.tkxel.com>
2024-07-10 13:21:33 +0500

    new button

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  const handleClick = () => {
+    alert('Button clicked!');
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Hello World</h1>
+        <button onClick={handleClick}>Click Me</button>
       </header>
     </div>
   );
