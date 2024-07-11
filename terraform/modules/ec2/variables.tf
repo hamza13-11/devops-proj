3d28d248bf994a4a5a87ccdbfb88c2704e150d39
hamza13-11 <hamza.ahmad@camp2.tkxel.com>
2024-07-11 14:29:15 +0500

    t2 medium

--- terraform/modules/ec2/variables.tf
+++ terraform/modules/ec2/variables.tf
@@ -6,7 +6,7 @@ variable "ami" {
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t2.small"
+  default     = "t2.medium"
 }
 
 variable "subnet_id" {
